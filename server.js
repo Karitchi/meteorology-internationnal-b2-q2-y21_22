5bc4fd94b7b661ecee23f35f80bc10787944c167
karitchi <karichikarichikarichi@gmail.com>
2022-02-23 11:35:51 +0100

    add send

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+app.use('/', (req, res) => {
+    res.send('hello')
+})
+
 app.get('/info', function (req, res) {
     res.send(req.query);
 });
