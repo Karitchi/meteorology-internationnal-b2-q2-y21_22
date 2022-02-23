1f3556feec17a9f8be6bf412174f4af59d95bfaa
karitchi <karichikarichikarichi@gmail.com>
2022-02-23 15:43:46 +0100

    add post route to add data to db

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require('express')
+const connectDb = require('./connectDb')
+const data = require('./models/Data')
+const app = express()
+const port = process.env.PORT || 3000
+// 3P1nHCg68z6FPYDx
 
+connectDb()
 
-app.get('/', function (req, res) {
-    res.send(req.query);
-});
+app.use(express.json())
+
+app.post('/', (req, res) => {
+    delete req.body._id;
+    const Data = new data({
+        ...req.body
+    });
+    Data.save()
+        .then(() => res.status(201).json({ message: 'object saved' }))
+        .catch(error => res.status(400).json({ error }));
+})
+
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
+    console.log(`listening on port ${port}`)
+})
